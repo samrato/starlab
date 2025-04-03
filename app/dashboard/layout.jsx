4b7d8ed72bd61c31eb8ae07b632b8c9bfd2a6190
Willington Juma <willingtonejumamconyango@gmail.com>
2025-04-03 14:23:55 +0300

    enter

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React from 'react'
+import Header from './_componets/Header'
 
-function DashboardLayout({ children }) {
-  return;
-  <div>{children}</div>;
+function DashboardLayout({children}) {
+  return (
+    <div className='mx-5 md:mx-20 lg:mx-36'>
+      <Header/>
+      {children}
+    </div>
+  )
 }
 
-export default DashboardLayout;
+export default DashboardLayout
